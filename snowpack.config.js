e60811324e83a8cd61edc2310720d62a4893fb8f
Diogo "devMozao" Fonseca <devmozao@outlook.com>
2021-01-21 01:33:16 -0300

    add react support

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    // directory name: 'build directory'
+   public: '/',
+   src: '/dist',
   },
   plugins: [
-    /* ... */
+  /* ... */
+    ['@snowpack/plugin-react-refresh'],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
